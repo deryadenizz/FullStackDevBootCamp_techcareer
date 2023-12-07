af282b0e6ba0c607718fc2b835b1df9d17e4af83
deryadenizz <derya.anaral@gmail.com>
2023-12-07 20:36:28 +0300

    API.Net Core basics

---
+++ BootcampApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BootcampApp.Models;
+
+namespace BootcampApp.Controllers;
+
+public class HomeController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+}
